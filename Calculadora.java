e5918018b007e614ca45ec3aeef874503ee6fca7
PacMor12 <159236958+PacMor12@users.noreply.github.com>
2024-03-09 19:59:07 +0100

    Clase Calculadora totalmente operativa
    Calculadora.java ha pasado todos los tests

--- Calculadora.java
+++ Calculadora.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package proyecto;
+package com.mycompany.mavenproject1;
+
 /**
- * Esta clase representa una calculadora simple que realiza operaciones básicas
  *
  * @author lopez
  */
@@ -14,46 +14,50 @@ public class Calculadora {
      * @param num1 El primer número a sumar.
      * @param num2 El segundo número a sumar.
      * devuelve la suma de los dos números.
+     * @return el resultado de la suma de los dos números
      */
-    public static void sumar (int num1, int num2){
+    public int sumar (int num1, int num2){
         int res;
         res = num1+num2;
         System.out.println("El resultado es: "+res);
+        return res;
     }
     
     /**
      * Este método resta dos números enteros.
      * @param num1 El primer número a restar.
      * @param num2 El segundo número a restar.
-     * devuelve la diferencia de los dos números.
-     */
-    public void restar (int num1, int num2){
+     * @return devuelve la diferencia de los dos números.    */
+    public int restar (int num1, int num2){
         int res;
         res = num1-num2;
         System.out.println("El resultado es: "+res);
+        return res;
     }
     
     /**
      * Este método multiplica dos números enteros.
      * @param num1 El primer número a multiplicar.
      * @param num2 El segundo número a multiplicar.
-     * devuelve la multiplicacion de los dos números.
+     * @return devuelve la multiplicacion de los dos números.
      */
-    public void multiplicar (int num1, int num2){
+    public int multiplicar (int num1, int num2){
         int res;
         res = num1*num2;
         System.out.println("El resultado es: "+res);
+        return res;
     }
     
     /**
      * Este método divide dos números enteros.
      * @param num1 El primer número a dividir.
      * @param num2 El segundo número a dividir.
-     * devuelve la division de los dos números.
+     * @return devuelve la division de los dos números.
      */
-        public void dividir (int num1, int num2){
+        public int dividir (int num1, int num2){
         int res;
         res = num1/num2;
         System.out.println("El resultado es: "+res);
+        return res;
     }
 }
