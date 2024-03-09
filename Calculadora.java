4b6ba4217c294b045bdcf8b8e780168bea68b6dd
FranMorales7 <162159997+FranMorales7@users.noreply.github.com>
2024-03-09 18:47:44 +0100

    Actualizacion con metodo restar ( )
    Al archivo Calculadora.java se le ha añadido el metodo restar

--- Calculadora.java
+++ Calculadora.java
@@ -2,34 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package proyecto_calculadora;
+package proyecto;
 
 /**
  *
  * @author lopez
  */
 public class Calculadora {
-    //atributo
-    private static int Resultado;
-    
-    public static void Sumar(int Cantidad1, int Cantidad2){
-        Resultado = Cantidad1+Cantidad2;
-        System.out.println(Resultado);
+    //Metodo para sumar dos enteros
+    public static void sumar (int num1, int num2){
+        int res;
+        res = num1+num2;
+        System.out.println("El resultado es: "+res);
     }
-    
-    public static void Restar(int Cantidad1, int Cantidad2){
-        Resultado = Cantidad1-Cantidad2;
-        System.out.println(Resultado);
-    }
-    
-    public static void Multiplicar(int Cantidad1, int Cantidad2){
-        Resultado = Cantidad1*Cantidad2;
-        System.out.println(Resultado);
-    }
-    
-    @Override
-    public String toString(){
-        return "El resultado es: "+Resultado;
+    public void restar (int num1, int num2){
+        int res;
+        res = num1-num2;
+        System.out.println("El resultado es: "+res);
     }
 }
-
