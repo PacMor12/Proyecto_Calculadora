60aae0e9b139540b88b24ca0e94f622b38ba4b9e
PacMor12 <159236958+PacMor12@users.noreply.github.com>
2024-03-09 18:44:20 +0100

    Subo la clase Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
 
+/**
+ *
+ * @author lopez
+ */
+public class Calculadora {
+    //Metodo para sumar dos enteros
+    public static void sumar (int num1, int num2){
+        int res;
+        res = num1+num2;
+        System.out.println("El resultado es: "+res);
+    }
+}
